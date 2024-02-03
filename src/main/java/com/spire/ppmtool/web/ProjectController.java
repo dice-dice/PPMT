5da381c125378e97eda275b46097fcd466393454
hatakeyama <z7daisuke.debzzha@gmail.com>
2024-02-03 13:11:41 +0900

    ProjectController and ProjectService changed

--- src/main/java/com/spire/ppmtool/web/ProjectController.java
+++ src/main/java/com/spire/ppmtool/web/ProjectController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/project")
@@ -33,6 +30,16 @@ public class ProjectController {
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 
     }
+
+    @GetMapping("{projectId}")
+    public ResponseEntity<?> getProjectId(@PathVariable String projectId) {
+
+        Project project = projectService.findProjectByIdentifier(projectId);
+
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
+
+    }
+
 
 
 }
